8f445d1b0954aecf84a35abd3e53e0fdd83dbda6
runaround <runaroundontheground@gmail.com>
2024-09-26 10:11:28 -0600

    whoops, realized i formatted my lists weird and chatgpt made weird code, so i removed it all and fixed the formatting Thu, Sep 26, 2024 10:11:28 AM

--- script.js
+++ script.js
@@ -2,90 +2,38 @@
 
 const listContainer = document.getElementById("listContainer");
 const taskContainer = document.getElementById("taskContainer");
-let currentList = "example task list name";
+let currentList = "example list name";
+// tasks/lists are only removed if you click some button on the task/list, marking them as done will not remove them
 const taskLists = {
-    "example task list name": [
-        {
-            "content": "example task content",
-            "done": false
-        }
-    ]
-}
-
-function updateContainers() {
-    // Clear existing content
-    listContainer.innerHTML = "";
-    taskContainer.innerHTML = "";
-
-    // Populate listContainer
-    for (const listName in taskLists) {
-        const listItem = document.createElement("div");
-        listItem.textContent = listName;
-        listItem.classList.add("list-item", "p-2", "border", "mb-2");
-        // make the id of the listItem be.. uhh
-        // the list title or whatever "example task list name"
-        // but remove all the spaces
-
-        // Highlight the current list
-        if (listName === currentList) listItem.classList.add("bg-primary", "text-white");
-
-        // Make list item clickable to switch to this task list
-        listItem.onclick = () => {
-            currentList = listName;
-            updateContainers();
-        };
-
-        listContainer.appendChild(listItem);
+    "example list name": {
+        "tasks": [
+            {
+                "content": "string which is a description of a task",
+                "done": false
+            }
+        ],
+        "done": false // if all the tasks within are cleared, use strikethrough on the list
     }
 
-    // Populate taskContainer
-    const tasks = taskLists[currentList];
-    tasks.forEach((task, index) => {
-        const taskDiv = document.createElement("div");
-        taskDiv.classList.add("d-flex", "align-items-center", "p-2", "border", "mb-2", "rounded");
+}
 
-        const checkbox = document.createElement("input");
-        checkbox.type = "checkbox";
-        checkbox.checked = task.done;
-        checkbox.classList.add("mr-2");
-        checkbox.onchange = () => {
-            task.done = checkbox.checked;
-        };
 
-        const taskContent = document.createElement("span");
-        taskContent.textContent = task.content;
-        taskContent.classList.add(task.done ? "text-decoration-line-through" : "");
+function updateContainers() {
 
-        taskDiv.appendChild(checkbox);
-        taskDiv.appendChild(taskContent);
-        taskContainer.appendChild(taskDiv);
-    });
 }
 
 function addList() {
-    const listName = prompt("Enter new list name:");
-    if (listName && !taskLists[listName]) {
-        taskLists[listName] = [];
-        currentList = listName;
-        updateContainers();
-    }
+
 }
 
 function addTask() {
-    const taskContent = prompt("Enter task content:");
-    if (taskContent) {
-        taskLists[currentList].push({ content: taskContent, done: false });
-        updateContainers();
-    }
+
 }
 
 function removeList() {
-    delete taskLists[currentList];
-    currentList = Object.keys(taskLists)[0] || ""; // Switch to another list or empty string
-    updateContainers();
+
 }
 
 function removeTask() {
-    delete taskLists[currentList]["example task list name"];
-    updateContainers();
+
 }
